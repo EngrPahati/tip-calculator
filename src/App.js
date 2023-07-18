aac0c55e378dfefb644b34a1a57455d5a2ac67c1
Moises Pahati <moisespahati@gmail.com>
2023-07-18 23:49:56 +0800

    add conditional rendering on Output and Reset Component

--- src/App.js
+++ src/App.js
@@ -28,15 +28,21 @@ export default function App() {
       >How did your friend like the service?
       </SelectPercentage>
 
-      <Output
-        bill={bill}
-        tip={tip}
-      />
-      <Reset
-        setBill={setBill}
-        setPercentage1={setPercentage1}
-        setPercentage2={setPercentage2}
-      />
+      {bill && (
+        <>
+          <Output
+            bill={bill}
+            tip={tip}
+          />
+          <Reset
+            setBill={setBill}
+            setPercentage1={setPercentage1}
+            setPercentage2={setPercentage2}
+          />
+        </>
+      )}
+
+
     </div>
   );
 }
@@ -64,7 +70,7 @@ function SelectPercentage({ children, onSelect, percentage}) {
         value={percentage}
         onChange={e => onSelect(Number(e.target.value))}
       >
-        <option selected={ percentage === 0 } value="0">Dissatisfied (0%)</option>
+        <option value="0">Dissatisfied (0%)</option>
         <option value="5">It was okay (5%)</option>
         <option value="10">It was good (10%)</option>
         <option value="20">Absolutely amazing! (20%)</option>
